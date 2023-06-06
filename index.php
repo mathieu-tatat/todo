7d37db1509598ba529823eb78f5f16657789d98a
mathieu-tatat <mathieu.tatat@laplateforme.io>
2023-06-06 11:59:37 +0200

    ajouter tache

--- index.php
+++ index.php
@@ -1,60 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <script type="text/javascript" src="index.js"></script>
-    <title>Document</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script type="text/javascript" src="index.js"></script>
+<link rel="stylesheet" href="index.css" type="text/css">
 </head>
 <body>
-    <div class="todo-content">
-  <h2 style="text-align:center">TO DO List</h2>
-  <div class="todo-input">
-    <input type="text" Placeholder="entrez une nouvelle tache">
-    <button class="add-todo">Ajouter</button>
-  </div>
-  
-<div class="error"></div>
-    <div class="todo-list" style="display:flex;justify-content:space-between"> 
-        <span>tache 1</span>
-        <label for="a faire">a faire</label>
-        <input type="checkbox" name="a faire"id="scales" name="scales">
-       
-        <label for="a faire">en cours</label>
-        <input type="checkbox" id="scales" name="scales">
-       
-        <label for="a faire">test</label>
-        <input type="checkbox" id="scales" name="scales">
-  
-
-        <label for="a faire">terminer</label>
-        <input type="checkbox" id="scales" name="scales">
-        <button>❌</button>
-        <button>⚙️</button>
-      
-
-    </div>
-    <div class="todo-list" style="display:flex;justify-content:space-between"> 
-        <span>tache 2</span>
-        <label for="a faire">a faire</label>
-        <input type="checkbox" name="a faire"id="scales" name="scales">
-       
-        <label for="a faire">en cours</label>
-        <input type="checkbox" id="scales" name="scales">
-       
-        <label for="a faire">test</label>
-        <input type="checkbox" id="scales" name="scales">
-  
-
-        <label for="a faire">terminer</label>
-        <input type="checkbox" id="scales" name="scales">
-        <button>❌</button>
-        <button>⚙️</button>
-      
-
-    </div>
+<!--===first todo list ====-->
+<div class="todo-content">
+<h2 style="text-align:center">TO DO List</h2>
+<div class="todo-input">
+<input type="text" Placeholder="Enter Something Here..">
+<button class="add-todo">Ajoutez</button>
 </div>
+<div class="error"></div>
+<div class="todo-list">
+</div>
+</div>
+<!--===first todo list ====-->
 </body>
 </html>
